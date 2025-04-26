f3483211ca7c7b6c8fb6c098d8263681c6a0ece5
BJ.A.J. <107040086+Bayraa0227@users.noreply.github.com>
2025-04-26 21:07:18 +0800

    repetitions аргументийг хэрэгжүүлэв

--- flashcard/src/main/java/com/example/fcsa311/a1/Flashcard.java
+++ flashcard/src/main/java/com/example/fcsa311/a1/Flashcard.java
@@ -8,6 +8,8 @@ import com.example.fcsa311.a1.organizer.MostMistakesFirstSorter;
 import com.example.fcsa311.a1.organizer.Random;
 import com.example.fcsa311.a1.organizer.RecentMistakesFirstSorter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -20,11 +22,25 @@ public class Flashcard {
     Options options = new Options();
     options.addOption("h", "help", false, "Тусламжийн мэдээлэл харуулах");
     options.addOption(
-        "o",
-        "order",
-        true,
-        "Эрэмбэлэгч (default: random). Сонголтууд:\n"
-            + "[random, worst-first, recent-mistakes-first]");
+        Option.builder()
+            .option("o")
+            .longOpt("order")
+            .hasArg()
+            .argName("order")
+            .desc(
+                "Эрэмбэлэгч (default: random). Сонголтууд:\n"
+                    + "[random, worst-first, recent-mistakes-first]")
+            .build());
+    options.addOption(
+        Option.builder()
+            .option("r")
+            .longOpt("repetitions")
+            .hasArg()
+            .argName("num")
+            .desc(
+                "Нэг картыг хэдэн удаа зөв хариулахыг шаардлага болгож тохируулна.\n"
+                    + "Хэрэв тодорхойлохгүй бол зөвхөн нэг удаа асууна.")
+            .build());
     options.addOption(null, "invert", false, "Картын асуулт, хариултыг сольж харуулах");
 
     CommandLineParser parser = new DefaultParser();
@@ -51,11 +67,23 @@ public class Flashcard {
     }
     String filePath = positional.get(0);
 
-    Scanner scanner = new Scanner(System.in);
     boolean invert = cmd.hasOption("invert");
     String orderOption = cmd.getOptionValue("order", "random");
+    int repetitions = 1;
+    if (cmd.hasOption("repetitions")) {
+      try {
+        repetitions = Integer.parseInt(cmd.getOptionValue("repetitions"));
+        if (repetitions < 1) {
+          throw new NumberFormatException();
+        }
+      } catch (NumberFormatException e) {
+        System.out.println("--repetitions тохиргооны аргумент эерэг бүхэл тоо байх ёстой.");
+        System.exit(1);
+      }
+    }
 
-    do {
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
       List<Card> cards;
       try {
         cards = CardLoader.loadFromFile(filePath);
@@ -104,20 +132,43 @@ public class Flashcard {
       }
       cards = organizer.organize(cards);
 
-      int correctCount = 0;
+      Map<Card, Integer> needed = new LinkedHashMap<>();
       for (Card card : cards) {
-        System.out.println("Асуулт: " + card.getQuestion());
-        System.out.print("Таны хариулт: ");
-        boolean correct = card.attempt(scanner.nextLine());
-        if (correct) {
-          System.out.println("Зөв байна!\n");
-          correctCount++;
-        } else {
-          System.out.println("Буруу байна. Зөв хариулт: " + card.getAnswer() + "\n");
+        needed.put(card, repetitions);
+      }
+
+      int totalCorrect = 0;
+      int totalQuestionAsked = 0;
+
+      while (!needed.isEmpty()) {
+        List<Card> roundCards = organizer.organize(new ArrayList<>(needed.keySet()));
+
+        for (Card card : roundCards) {
+          int rem = needed.get(card);
+          System.out.println("Асуулт: " + card.getQuestion());
+          System.out.print("Таны хариулт: ");
+          boolean correct = card.attempt(scanner.nextLine());
+          totalQuestionAsked++;
+          if (correct) {
+            totalCorrect++;
+            rem--;
+            System.out.println("Зөв байна!\n");
+            if (rem <= 0) {
+              needed.remove(card);
+            } else {
+              needed.put(card, rem);
+            }
+          } else {
+            System.out.println("Буруу байна. Зөв хариулт: " + card.getAnswer() + "\n");
+          }
+          if (needed.isEmpty()) {
+            break;
+          }
         }
       }
 
-      System.out.printf("Бүх асуултанд хариуллаа. %d-с %d зөв.%n", cards.size(), correctCount);
+      System.out.printf(
+          "Бүх асуултанд хариуллаа. %d-с %d зөв хариулсан.%n", totalQuestionAsked, totalCorrect);
 
       UserDataManager.saveStatuses(filePath, cards);
 
@@ -127,6 +178,6 @@ public class Flashcard {
         scanner.close();
         break;
       }
-    } while (true);
+    }
   }
 }
